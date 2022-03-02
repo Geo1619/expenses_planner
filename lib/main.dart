53a4775243af49be15e8539594166f18c0751b90
Geo1619 <gi0rg0sdimitr1988@gmail.com>
2022-03-02 17:08:27 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import './transaction.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const <Widget>[
+          SizedBox(
+            width: double.infinity,
+            child: Card(
+              color: Colors.amberAccent,
+              child: Text('Chart!'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text('List of TX!'),
+          ),
+        ],
+      ),
+    );
+  }
+}
