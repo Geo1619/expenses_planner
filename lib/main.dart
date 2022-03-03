eb28de1738aa2a318644402b2e27d450d128b423
Geo1619 <gi0rg0sdimitr1988@gmail.com>
2022-03-03 17:02:42 +0200

    Input Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import './transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(const MyApp());
 
@@ -16,16 +17,24 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
+  MyHomePage({Key? key}) : super(key: key);
   final List<Transaction> transactions = [
     Transaction(
       id: 't1',
       title: 'New Shoes',
       amount: 69.99,
       date: DateTime.now(),
-    )
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
   ];
+
+  String titleInput = '';
+  String amountInput = '';
 
   @override
   Widget build(BuildContext context) {
@@ -34,10 +43,10 @@ class MyHomePage extends StatelessWidget {
         title: const Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        // mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: const <Widget>[
-          SizedBox(
+        children: <Widget>[
+          const SizedBox(
             width: double.infinity,
             child: Card(
               color: Colors.amberAccent,
@@ -46,7 +55,81 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           Card(
-            child: Text('List of TX!'),
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    onChanged: (val) {
+                      titleInput = val;
+                    },
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                    onChanged: (val) {
+                      amountInput = val;
+                    },
+                  ),
+                  TextButton(
+                    child: const Text('Add Transaction'),
+                    onPressed: () {
+                      print('Title Input: $titleInput');
+                      print('Amount Input: $amountInput');
+                    },
+                    style: TextButton.styleFrom(
+                      primary: Colors.purple,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Column(
+            children: transactions
+                .map((tx) => Card(
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 10,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.yellowAccent,
+                              border: Border.all(
+                                color: Colors.purple,
+                                width: 2,
+                              ),
+                            ),
+                            padding: const EdgeInsets.all(10),
+                            child: Text(
+                              '\$${tx.amount}',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                tx.title,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 16),
+                              ),
+                              Text(DateFormat.yMMMd().format(tx.date),
+                                  style: const TextStyle(color: Colors.grey))
+                            ],
+                          )
+                        ],
+                      ),
+                    ))
+                .toList(),
           ),
         ],
       ),
